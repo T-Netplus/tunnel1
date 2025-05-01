46df5b246090c8e29a672e6bcab0caf4fb565564
Tnet+ <138398697+T-Netplus@users.noreply.github.com>
2025-05-01 18:10:15 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,173 @@
+z="
+";vz='o.ve';oEz='nfo ';bDz='esac';dBz='info';nEz='memi';ABz='_nam';VEz='p Sw';tDz='(una';jBz='l_mt';PEz='0) "';HBz='2=$(';wz='ndor';fBz='l_un';BBz='e)';MDz='harg';bz='.ven';UBz='core';PBz='ipna';bBz='proc';yz='.soc';mEz='roc/';OCz='$2}'\''';TCz='batt';eCz='wk '\''';pz='ardw';kDz=' tr ';lBz='del)';MBz='del.';Yz='actu';CDz='atus';ZBz='cess';SBz='pnam';FDz='tatu';nDz=' | w';DEz='e | ';Kz='manu';uBz='prop';rDz='ersi';oBz='r.qt';UCz='ery_';VCz='leve';dz='soc.';Sz='nufa';OBz='o.ch';QBz='me)';Jz='cpu_';NCz='int ';QEz=' men';rz='chip';nCz=' bat';gCz='nt $';GDz='s="U';kz='ture';QDz='s="D';cDz='sens';sCz=' awk';tBz='(get';yDz='(cat';ZCz='ery ';iz='_man';nz='rop ';bEz='o | ';mCz='psys';lz='r=$(';uEz='" MB';tCz=' '\''{p';iCz='"}'\'')';pDz='kern';LCz='R==2';vCz=' $2}';uCz='rint';kCz='us=$';qz='are.';AEz=' /pr';uz='name';GEz='int(';xBz='uct.';XDz='5) s';jEz=' Swa';jz='ufac';Bz='d_br';ZDz='ull"';yBz='oplu';GCz='df -';jCz='stat';LEz=' int';UDz='4) s';mDz=''\''\n'\''';UEz='(gre';QCz='labl';JEz=' " j';iBz='el)';Nz='=$(g';bCz='ep l';az='p ro';gDz='rdwa';dCz=' | a';hCz='2 "%';Lz='fact';GBz='nal_';MCz=' {pr';DBz='.mod';cBz='/cpu';RBz='_chi';vBz=' ro.';CCz='o)';Vz='vend';pBz='i.so';BCz='uinf';oCz='tery';aCz='| gr';Pz='op r';wBz='prod';tz='qti_';KCz='k '\''N';NBz='part';rCz='us |';Fz='p Bu';gEz='free';CEz='ptim';xz='.qti';Gz='ild.';BDz='y_st';XEz='tal ';EDz='1) s';WEz='apTo';FEz=''\''{pr';ECz='l_st';fDz='o.ha';TDz='ng" ';Wz='or_m';oDz='c -l';Rz='c.ma';DDz=' in';qDz='el_v';hDz='re.s';KDz='2) s';pEz='k '\''{';hBz='sal=';eDz='ount';Zz='rer=';qEz='prin';tEz='024 ';YDz='s="F';RDz='isch';JDz=';;';XCz='dump';wDz='upti';kEz='pFre';Mz='urer';fCz='{pri';nBz='i=$(';gBz='iver';FCz='orag';HCz='h /d';sEz=' / 1';YCz='sys ';ACz='s.cp';RCz='e_st';TBz='e=$(';Uz='er)';Ez='tpro';HDz='nkno';ADz='tter';aDz='*) s';CBz='mtk_';SCz='$4}'\''';TEz='ap=$';fz='hard';VBz='s=$(';Qz='o.so';WCz='l=$(';SDz='argi';wCz=''\'')';EEz='awk ';DCz='tota';hEz='_swa';ez=')';Oz='etpr';cz='dor.';pCz=' | g';eEz='4 " ';HEz='$1/3';sDz='on=$';Cz='and=';hz='_soc';dEz=' 102';mz='getp';aBz='or /';JBz='endo';PCz='avai';gz='ware';dDz='or_c';LBz='c.mo';ZEz='c/me';iEz='p=$(';vDz='r)';Az='buil';xCz='case';IDz='wn" ';jDz='rs |';JCz='| aw';YEz='/pro';rEz='t $2';rBz='l_op';REz='it"}';Iz='D)';lCz='(dum';Tz='ctur';oz='ro.h';yCz=' $ba';Xz='anuf';qBz='c_mo';xDz='me=$';kBz='k=$(';fEz='MB"}';SEz='l_sw';eBz='mode';NDz='ing"';sz='set_';IBz='ro.v';FBz='xter';iDz='enso';NEz='%360';ICz='ata ';mBz='l_qt';VDz='s="N';KBz='r.so';BEz='oc/u';WDz='ot C';OEz='0)/6';lEz='e /p';Hz='BRAN';Dz='$(ge';ODz=' ;;';PDz='3) s';WBz='grep';aEz='minf';LDz='s="C';qCz='rep ';KEz='am "';cCz='evel';YBz='^pro';lDz=''\'','\'' ';uDz='me -';IEz='600)';EBz='el.e';cEz='$2 /';XBz=' -c ';MEz='(($1';sBz='po=$';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$z$Jz$Vz$Wz$Xz$Yz$Zz$Dz$Ez$az$bz$cz$dz$Kz$Lz$Mz$ez$z$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$dz$Kz$Lz$Mz$ez$z$rz$sz$tz$uz$Nz$Oz$Pz$vz$wz$xz$yz$ABz$BBz$z$rz$sz$CBz$uz$Nz$Oz$Pz$vz$wz$yz$DBz$EBz$FBz$GBz$uz$ez$z$rz$sz$CBz$uz$HBz$mz$nz$IBz$JBz$KBz$LBz$MBz$NBz$ABz$BBz$z$Jz$rz$uz$Nz$Oz$Pz$OBz$PBz$QBz$z$Jz$fz$gz$RBz$SBz$TBz$mz$nz$oz$pz$qz$rz$uz$ez$z$Jz$UBz$VBz$WBz$XBz$YBz$ZBz$aBz$bBz$cBz$dBz$ez$z$Jz$eBz$fBz$gBz$hBz$Dz$Ez$az$yz$DBz$iBz$z$Jz$eBz$jBz$kBz$mz$nz$IBz$JBz$KBz$LBz$lBz$z$Jz$eBz$mBz$nBz$mz$nz$IBz$JBz$oBz$pBz$qBz$lBz$z$Jz$eBz$rBz$sBz$tBz$uBz$vBz$wBz$xBz$yBz$ACz$BCz$CCz$z$DCz$ECz$FCz$TBz$GCz$HCz$ICz$JCz$KCz$LCz$MCz$NCz$OCz$ez$z$PCz$QCz$RCz$FCz$TBz$GCz$HCz$ICz$JCz$KCz$LCz$MCz$NCz$SCz$ez$z$TCz$UCz$VCz$WCz$XCz$YCz$TCz$ZCz$aCz$bCz$cCz$dCz$eCz$fCz$gCz$hCz$iCz$z$TCz$UCz$jCz$kCz$lCz$mCz$nCz$oCz$pCz$qCz$jCz$rCz$sCz$tCz$uCz$vCz$wCz$z$xCz$yCz$ADz$BDz$CDz$DDz$z$EDz$FDz$GDz$HDz$IDz$JDz$z$KDz$FDz$LDz$MDz$NDz$ODz$z$PDz$FDz$QDz$RDz$SDz$TDz$JDz$z$UDz$FDz$VDz$WDz$MDz$NDz$ODz$z$XDz$FDz$YDz$ZDz$ODz$z$aDz$FDz$GDz$HDz$IDz$JDz$z$bDz$z$cDz$dDz$eDz$Nz$Oz$Pz$fDz$gDz$hDz$iDz$jDz$kDz$lDz$mDz$nDz$oDz$ez$z$pDz$qDz$rDz$sDz$tDz$uDz$vDz$z$wDz$xDz$yDz$AEz$BEz$CEz$DEz$EEz$FEz$NCz$GEz$HEz$IEz$JEz$KEz$LEz$MEz$NEz$OEz$PEz$QEz$REz$wCz$z$DCz$SEz$TEz$UEz$VEz$WEz$XEz$YEz$ZEz$aEz$bEz$EEz$FEz$NCz$cEz$dEz$eEz$fEz$wCz$z$gEz$hEz$iEz$WBz$jEz$kEz$lEz$mEz$nEz$oEz$JCz$pEz$qEz$rEz$sEz$tEz$uEz$iCz"
+echo ""
+echo "┌─┐┌─┐┌───┐┌─┐┌─┐┌───┐┌───┐ 
+││└┘│││┌─┐│└┐└┘┌┘│┌─┐││┌─┐│ 
+│┌┐┌┐│││─││─└┐┌┘─│└─┘││└──┐ 
+│││││││└─┘│─┌┘└┐─│┌──┘└──┐│ 
+│││││││┌─┐│┌┘┌┐└┐││───│└─┘│ 
+└┘└┘└┘└┘─└┘└─┘└─┘└┘───└───┘ 
+"
+    sleep 1
+    echo "-------------------------------------------------------------------------"
+    echo "  • INFORMASI DEVICE"
+    echo "-------------------------------------------------------------------------"
+    sleep 0.5
+    echo "  • ANDROID SDK        ➜ $(getprop ro.build.version.sdk)"
+    sleep 0.5
+    echo "  • ANDROID VERSION    ➜ $(getprop ro.build.version.release)"
+    sleep 0.5
+    echo "  • DEVICE MODEL       ➜ $(getprop ro.product.model)"
+    sleep 0.5
+    echo "  • BRAND              ➜ $(getprop ro.product.brand)"
+    sleep 0.5
+    echo "  • NAMA DEVICE        ➜ $(getprop persist.sys.devicename)"
+    sleep 0.5
+    echo "  • HARDWARE           ➜ $(getprop ro.hardware)"
+    sleep 0.5
+    echo "  • GPU                ➜ $(getprop ro.opengles.version)"
+    sleep 0.5
+    echo "  • CPU                ➜ $(getprop ro.product.cpu.abi)"
+    sleep 0.5
+    echo "  • RAM                ➜ $(cat /proc/meminfo | grep MemTotal | awk '{print $2}')"
+    sleep 0.5
+    echo "  • RAM TERSEDIA       ➜ $(grep MemAvailable /proc/meminfo | awk '{print $2 / 1024 " MB"}')"
+    sleep 0.5
+    echo "  • MANUFACTURER       ➜ $cpu_manufacturer $cpu_vendor_manufacturer $hardware_soc_manufacturer"
+    sleep 0.5
+    echo "  • SOC NAME           ➜$chipset_qti_name $chipset_mtk_name $chipset_mtk_name2 $cpu_chipname $cpu_hardware_chipname"
+    sleep 0.5
+    echo "  • SOC MODEL          ➜ $cpu_model_universal $cpu_model_mtk $cpu_model_qti $cpu_model_oppo"    
+    sleep 0.5
+    echo "  • PENYIMPANAN TOTAL  ➜ $total_storage"
+    sleep 0.5
+    echo "  • PENYIMPANAN        ➜ $available_storage"
+    sleep 0.5
+    echo "  • VIRTUAL RAM TOTAL  ➜ $total_swap"
+    sleep 0.5
+    echo "  • VIRTUAL RAM BEBAS  ➜ $free_swap"
+    sleep 0.5
+    echo "  • BATERAI            ➜ $battery_level ($status)"
+    sleep 0.5
+    echo "  • JUMLAH SENSOR      ➜ $sensor_count"
+    sleep 0.5
+    echo "  • VERSI KERNEL       ➜ $kernel_version"
+    sleep 0.5
+    echo "  • UPTIME             ➜ $uptime"
+    sleep 0.5
+    echo "-------------------------------------------------------------------------"
+    echo ""
+    echo "-------------------------------------------------------------------------"
+    echo "  • INFORMASI MODULE"
+    echo "-------------------------------------------------------------------------"
+    sleep 0.5
+    echo "  • DEVELOPER          ➜ @traatweak"
+    sleep 0.5
+    echo "  • VERSION MODULE     ➜ 1.3"
+    sleep 0.5
+    echo "  • FUNGSI MODULE      ➜ MEMAKSIMALKAN FPS KE 60-144"
+    echo "-------------------------------------------------------------------------"
+    sleep 0.5
+    echo ""
+    echo "-------------------------------------------------------------------------"
+    echo "- LIST FITUR 💥"
+    echo "-------------------------------------------------------------------------"
+sleep 0.5
+echo "- MEMAKSIMALKAN FPS"
+echo "- UNLOCK FPS 60-144 FPS"
+echo "-------------------------------------------------------------------------"
+sleep 0.5
+echo "- Menginstall Module.."
+echo "-------------------------------------------------------------------------"
+
+##################################################################
+(
+cmd device_config put system display_refresh_rate 60
+settings put secure user_refresh_rate 60
+settings put secure miui_refresh_rate 60
+settings put system min_frame_rate 60
+settings put system max_frame_rate 60
+settings put system tran_refresh_mode 60
+settings put system last_tran_refresh_mode_in_refresh_setting 60
+settings put global min_fps 60
+settings put global max_fps 60
+settings put system tran_need_recovery_refresh_mode 60
+settings put system display_min_refresh_rate 60
+settings put system min_refresh_rate 60
+settings put system max_refresh_rate 60
+settings put system peak_refresh_rate 60
+settings put secure refresh_rate_mode 60
+settings put system user_refresh_rate 60
+settings put system thermal_limit_refresh_rate 60
+settings put system NV_FPSLIMIT 60
+settings put system fps.limit.is.now locked
+) > /dev/null 2>&1 &
+(
+cmd device_config put system display_refresh_rate 90
+settings put secure user_refresh_rate 90
+settings put secure miui_refresh_rate 90
+settings put system min_frame_rate 90
+settings put system max_frame_rate 90
+settings put system tran_refresh_mode 90
+settings put system last_tran_refresh_mode_in_refresh_setting 90
+settings put global min_fps 90
+settings put global max_fps 90
+settings put system tran_need_recovery_refresh_mode 90
+settings put system display_min_refresh_rate 90
+settings put system min_refresh_rate 90
+settings put system max_refresh_rate 90
+settings put system peak_refresh_rate 90
+settings put secure refresh_rate_mode 90
+settings put system user_refresh_rate 90
+settings put system thermal_limit_refresh_rate 90
+settings put system NV_FPSLIMIT 90
+settings put system fps.limit.is.now locked
+) > /dev/null 2>&1 &
+(
+cmd device_config put system display_refresh_rate 120
+settings put secure user_refresh_rate 120
+settings put secure miui_refresh_rate 120
+settings put system min_frame_rate 120
+settings put system max_frame_rate 120
+settings put system tran_refresh_mode 120
+settings put system last_tran_refresh_mode_in_refresh_setting 120
+settings put global min_fps 120
+settings put global max_fps 120
+settings put system tran_need_recovery_refresh_mode 120
+settings put system display_min_refresh_rate 120 
+settings put system min_refresh_rate 120
+settings put system max_refresh_rate 120
+settings put system peak_refresh_rate 120
+settings put secure refresh_rate_mode 120
+settings put system user_refresh_rate 120
+settings put system thermal_limit_refresh_rate 120
+settings put system NV_FPSLIMIT 120
+settings put system fps.limit.is.now locked
+) > /dev/null 2>&1 &
+(
+cmd device_config put system display_refresh_rate 144
+settings put secure user_refresh_rate 144
+settings put secure miui_refresh_rate 144
+settings put system min_frame_rate 144
+settings put system max_frame_rate 144
+settings put system tran_refresh_mode 144
+settings put system last_tran_refresh_mode_in_refresh_setting 144
+settings put global min_fps 144
+settings put global max_fps 144
+settings put system tran_need_recovery_refresh_mode 144
+settings put system display_min_refresh_rate 144 
+settings put system min_refresh_rate 144
+settings put system max_refresh_rate 144
+settings put system peak_refresh_rate 144
+settings put secure refresh_rate_mode 144
+settings put system user_refresh_rate 144
+settings put system thermal_limit_refresh_rate 144
+settings put system NV_FPSLIMIT 144
+settings put system fps.limit.is.now locked
+) > /dev/null 2>&1 &
+cmd notification post -S bigtext -t 'MAX-FPS 💥' 'Tag' "Max fps active⚡" > /dev/null 2>&1
+##################################################################
+echo "-------------------------------------------------------------------------"
+echo " - DONE"
+echo "-------------------------------------------------------------------------"
